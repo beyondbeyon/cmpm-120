c75dd29c9839591da25206ea4b95080897511521
Bryon Anderson <brkander@ucsc.edu>
2023-11-05 21:46:09 -0800

    adding in endless Runner

---
+++ nutdash/src/scenes/Menu.js
@@ -0,0 +1,48 @@
+class Menu extends Phaser.Scene {
+    constructor() {
+        super("menuScene");
+    }
+  
+    create() {
+        // menu text configuration
+        let menuConfig = {
+            fontFamily: 'Courier',
+            fontSize: '28px',
+            backgroundColor: '#F3B141',
+            color: '#843605',
+            align: 'right',
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWidth: 0
+        }
+  
+        game.input.mouse.capture = true;
+        
+        // show menu text
+        this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'Nut Dash: Leave my nuts alone!', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/2, 'Use ←→ arrows to move & (F) to fire', menuConfig).setOrigin(0.5);
+        menuConfig.backgroundColor = '#00FF00';
+        menuConfig.color = '#000';
+        this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding, 'Press ← for Novice or → for Expert', menuConfig).setOrigin(0.5);
+        
+  
+        // define keys
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+    }
+  
+    update() {
+        if (Phaser.Input.Keyboard.JustDown(keyLEFT)) {
+          // Novice mode
+          this.scene.start("playScene");    
+        }   
+        if (Phaser.Input.Keyboard.JustDown(keyRIGHT)) {
+          // Expert mode
+          gameOptions.platformStartSpeed = gameOptions.platformStartSpeed * 1.5,
+          this.scene.start("playScene");    
+        }
+      }
+  }
+
